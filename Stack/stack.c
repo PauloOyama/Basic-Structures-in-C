bc01f6296c161f3e91dcca85141b16f3896716a4
Paulo Oyama <papaloyama@gmail.com>
2022-09-03 23:34:58 -0300

    Basic Stack Structure

---
+++ Stack/stack.c
@@ -0,0 +1,20 @@
+#include "stack.h"
+
+struct Stack
+{
+    int *value;
+    struct Stack *next;
+};
+
+void init_stack()
+{
+    return NULL;
+}
+
+int push(Stack)
+{
+}
+
+int pop(Stack *stck)
+{
+}
---
+++ Stack/stack.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+typedef struct stck *Stack;
+// int push();
+int *pop();
+// int *peek();
+int is_empty();
+// int *is_full();
+// int *print_stack();
+
+#endif
